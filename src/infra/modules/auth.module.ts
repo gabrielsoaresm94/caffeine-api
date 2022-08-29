e7d8452b20cd1040b129c6fc0fe8ca67864b0b13
Gabriel Soares <gabrielsoaresm94@gmail.com>
2022-08-29 15:33:13 -0300

    fix: add imports modules

--- src/infra/modules/auth.module.ts
+++ src/infra/modules/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from 'src/adapters/presentation/controllers/auth.controller';
+import { UsersUseCase } from 'src/core/usecases/user.usecase';
+import { PostgresUsersRepository } from '../database/postgres/repositories/users.repository';
 
 @Module({
   imports: [],
   controllers: [AuthController],
-  providers: [],
+  providers: [UsersUseCase, PostgresUsersRepository],
 })
 export class AuthModule {}
--- src/infra/modules/users.module.ts
+++ src/infra/modules/users.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from 'src/adapters/presentation/controllers/users.controller';
+import { UsersUseCase } from 'src/core/usecases/user.usecase';
+import { PostgresUsersRepository } from '../database/postgres/repositories/users.repository';
 
 @Module({
   imports: [],
   controllers: [UsersController],
-  providers: [],
+  providers: [UsersUseCase, PostgresUsersRepository],
 })
 export class UsersModule {}
 
