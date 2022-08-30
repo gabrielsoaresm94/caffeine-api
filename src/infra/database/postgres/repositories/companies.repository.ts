290646188cdb4a5e1ee576afa811f9407e322f10
Gabriel Soares <gabrielsoaresm94@gmail.com>
2022-08-30 11:49:09 -0300

    feat: add db docker and queries

--- src/infra/database/postgres/repositories/companies.repository.ts
+++ src/infra/database/postgres/repositories/companies.repository.ts
@@ -9,21 +9,21 @@ export class PostgresCompaniesRepository {
   ): Promise<ICompanyData> {
     const data = company.toPlain();
     const postCompany = await client.query(
-      `INSERT INTO companies (Id, Cnpj, Name, SocialReason, Email, Phone, Information, ShopmanId) VALUES (${data.id},'${data.cnpj}', '${data.name}','${data.socialReason}', '${data.email}','${data.phone},'${data.information},'${shopmanId}') RETURNING *`,
+      `INSERT INTO Companies (Id, Cnpj, Name, SocialReason, Email, Phone, Information, ShopmanId) VALUES (${data.id},'${data.cnpj}', '${data.name}','${data.socialReason}', '${data.email}','${data.phone},'${data.information},'${shopmanId}') RETURNING *`,
     );
     const companySaved = postCompany.rows[0] as ICompanyData;
     return companySaved;
   }
 
   async listCompanies(): Promise<ICompanyData[]> {
-    const queryCompanies = await client.query('SELECT * from companies');
+    const queryCompanies = await client.query('SELECT * from Companies');
     const companies = queryCompanies.rows as ICompanyData[];
     return companies;
   }
 
   async listCompaniesByCNPJ(cnpj: string): Promise<ICompanyData[]> {
     const queryCompanies = await client.query(
-      `SELECT * from companies WHERE cnpj = ${cnpj}`,
+      `SELECT * from Companies WHERE Cnpj = ${cnpj}`,
     );
     const companies = queryCompanies.rows as ICompanyData[];
     return companies;
@@ -31,7 +31,7 @@ export class PostgresCompaniesRepository {
 
   async findCompanyById(id: string): Promise<ICompanyData> {
     const queryCompany = await client.query(
-      `SELECT * FROM companies WHERE id = ${id}`,
+      `SELECT * FROM Companies WHERE Id = ${id}`,
     );
     const company = queryCompany.rows[0] as ICompanyData;
     return company;
--- src/infra/database/postgres/repositories/users.repository.ts
+++ src/infra/database/postgres/repositories/users.repository.ts
@@ -6,21 +6,21 @@ export class PostgresUsersRepository {
   async createUser(user: User): Promise<IUserData> {
     const data = user.toPlain();
     const postUser = await client.query(
-      `INSERT INTO users (Id, Name, Role, Email, Password, Phone1, Phone2) VALUES (${data.id},'${data.name}','${data.role}', '${data.email}','${data.password}', '${data.phone_1}', '${data.phone_2}') RETURNING *`,
+      `INSERT INTO Users (Id, Name, Role, Email, Password, Phone1, Phone2) VALUES (${data.id},'${data.name}','${data.role}', '${data.email}','${data.password}', '${data.phone_1}', '${data.phone_2}') RETURNING *`,
     );
     const userSaved = postUser.rows[0] as IUserData;
     return userSaved;
   }
 
   async listUsers(): Promise<IUserData[]> {
-    const queryUsers = await client.query('SELECT * from users');
+    const queryUsers = await client.query('SELECT * from Users');
     const users = queryUsers.rows as IUserData[];
     return users;
   }
 
   async findUserByEmail(email: string): Promise<IUserData> {
     const queryUser = await client.query(
-      `SELECT * FROM users WHERE email = ${email}`,
+      `SELECT * FROM Users WHERE email = ${email}`,
     );
     const user = queryUser.rows[0] as IUserData;
     return user;
@@ -28,7 +28,7 @@ export class PostgresUsersRepository {
 
   async findUserById(id: string): Promise<IUserData> {
     const queryUser = await client.query(
-      `SELECT * FROM users WHERE id = ${id}`,
+      `SELECT * FROM Users WHERE id = ${id}`,
     );
     const user = queryUser.rows[0] as IUserData;
     return user;
